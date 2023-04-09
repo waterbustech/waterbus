ed74b2617f57199db04de0aa71e23718b6502af2
lambiengcode <lambiengcode@gmail.com>
2023-04-09 16:06:37 +0700

    feat(home): add invitation list in home screen

--- lib/features/home/widgets/invitation_list.dart
+++ lib/features/home/widgets/invitation_list.dart
@@ -5,21 +5,20 @@ import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
 // Project imports:
-import 'package:waterbus/features/home/widgets/meeting_card.dart';
+import 'package:waterbus/features/home/widgets/invitation_card.dart';
 
-class MyMeetings extends StatelessWidget {
-  const MyMeetings({super.key});
+class InvitationList extends StatelessWidget {
+  const InvitationList({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 14.sp),
       padding: EdgeInsets.symmetric(horizontal: 12.sp),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "Last Meetings",
+            "Invitations",
             style: Theme.of(context).textTheme.bodyLarge?.copyWith(
                   fontSize: 14.sp,
                   fontWeight: FontWeight.w600,
@@ -29,10 +28,10 @@ class MyMeetings extends StatelessWidget {
           ListView.builder(
             physics: const NeverScrollableScrollPhysics(),
             shrinkWrap: true,
-            padding: EdgeInsets.only(bottom: 40.sp),
-            itemCount: 2,
+            padding: EdgeInsets.zero,
+            itemCount: 1,
             itemBuilder: (context, index) {
-              return const MeetingCard();
+              return const InvitationCard();
             },
           ),
         ],
