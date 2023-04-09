67ce1eb70e20fe59e1863596c4869a17a6909452
lambiengcode <lambiengcode@gmail.com>
2023-04-09 07:07:20 +0700

    import_sorter

--- lib/core/injection/injection_container.config.dart
+++ lib/core/injection/injection_container.config.dart
@@ -9,6 +9,8 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+
+// Package imports:
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
