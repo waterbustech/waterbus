848ef76bf97d8077d4ca1fb2e3a1514bc71a491d
lambiengcode <lambiengcode@gmail.com>
2023-04-10 13:19:03 +0700

    init(screens): schedule, notifications and account

---
+++ lib/features/notifications/screens/notifications_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotificationsScreen extends StatefulWidget {
+  const NotificationsScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _NotificationsScreenState();
+}
+
+class _NotificationsScreenState extends State<NotificationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
