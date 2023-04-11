6e1317250f2c7c5c6cbedfe25e7fdf7e5797f50d
lambiengcode <lambiengcode@gmail.com>
2023-04-11 19:46:08 +0700

    update .

---
+++ lib/features/meeting/screens/meeting_screen.dart
@@ -0,0 +1,18 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+class MeetingScreen extends StatefulWidget {
+  const MeetingScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MeetingScreenState();
+}
+
+class _MeetingScreenState extends State<MeetingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
