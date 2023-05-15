4a5ca54030e36198bdce98c69e974d24d8a1e603
lambiengcode <lambiengcode@gmail.com>
2023-05-15 12:24:35 +0700

    import_sorter

--- lib/features/profile/data/datasources/user_remote_datasource.dart
+++ lib/features/profile/data/datasources/user_remote_datasource.dart
@@ -1,8 +1,8 @@
 // Package imports:
 import 'package:injectable/injectable.dart';
-import 'package:waterbus/core/constants/api_endpoints.dart';
 
 // Project imports:
+import 'package:waterbus/core/constants/api_endpoints.dart';
 import 'package:waterbus/core/utils/datasources/base_remote_data.dart';
 import 'package:waterbus/features/auth/data/models/user_model.dart';
 
