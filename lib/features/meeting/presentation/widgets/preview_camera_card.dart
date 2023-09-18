cc6cda6a7fd912cebb764e9979351f1f32aeded8
lambiengcode <lambiengcode@gmail.com>
2023-09-18 23:00:52 +0700

    import_sorter

--- lib/features/meeting/presentation/widgets/preview_camera_card.dart
+++ lib/features/meeting/presentation/widgets/preview_camera_card.dart
@@ -1,6 +1,11 @@
+// Flutter imports:
 import 'package:flutter/material.dart';
+
+// Package imports:
 import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
 import 'package:sizer/sizer.dart';
+
+// Project imports:
 import 'package:waterbus/features/meeting/presentation/widgets/call_action_button.dart';
 
 class PreviewCameraCard extends StatelessWidget {
