e22c2717c2fe9f956f58ea827085eb29469caa30
lambiengcode <lambiengcode@gmail.com>
2023-10-10 13:42:34 +0700

    feat(webrtc): enable audio dtx

--- lib/services/webrtc/helpers/webrtc_utils.dart
+++ lib/services/webrtc/helpers/webrtc_utils.dart
@@ -1,15 +1,1 @@
-class WebRTCUtils {
-  static String removeRTCPMux(String sdp) {
-    sdp = sdp.replaceAll(RegExp(r'a=rtcp-mux.*\r\n'), '');
-
-    return sdp;
-  }
-
-  static String enableAudioDTX({required String sdp}) {
-    sdp = sdp.replaceAll(
-      'a=fmtp:111 minptime=10;useinbandfec=1',
-      'a=fmtp:111 minptime=10;useinbandfec=1;usedtx=1',
-    );
-    return sdp;
-  }
-}
+class WebRTCUtils {}
