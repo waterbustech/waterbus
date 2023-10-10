4d75d489b681c715f5c4c726a169338ff92f0060
lambiengcode <lambiengcode@gmail.com>
2023-10-10 22:37:50 +0700

    import_sorter

--- lib/services/webrtc/models/participant_sfu.dart
+++ lib/services/webrtc/models/participant_sfu.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first, must_be_immutable
+
 // Package imports:
 import 'package:equatable/equatable.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
