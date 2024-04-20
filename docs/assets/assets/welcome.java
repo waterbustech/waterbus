049858b8a739402aca48a0253f8017edf2e5b067
lambiengcode <lambiengcode@gmail.com>
2024-04-20 21:12:51 +0700

    chore: build web

---
+++ docs/assets/assets/welcome.java
@@ -0,0 +1,5 @@
+/*
+💙 Thank you for using Waterbus. Let's enjoy your time.
+
+If you have suggestions or problems, please open an issue or contribute directly at https://github.com/lambiengcode/waterbus
+*/
