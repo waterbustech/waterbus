aa778c21177896e67c4c2f4fd6760fd14da2c642
lambiengcode <lambiengcode@gmail.com>
2024-07-11 19:55:18 +0700

    fix(linux): firebase authentication fail

--- packages/auth/lib/services/index.dart
+++ packages/auth/lib/services/index.dart
@@ -1,7 +1,11 @@
+import 'package:auth/services/auth_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:universal_io/io.dart';
 
-export './auth_service_impl_stub.dart'
-    if (isLinux) './auth_service_impl_linux.dart';
+import './auth_service_impl_stub.dart' as sub;
+import './auth_service_impl_linux.dart' as linux;
 
 bool get isLinux => !kIsWeb && Platform.isLinux;
+
+AuthService get getInstance =>
+    isLinux ? linux.AuthServiceImpl() : sub.AuthServiceImpl();
