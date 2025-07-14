5e5c8791f39a9df370c0961c224d9b2448cfa4ee
Kai <lambiengcode@gmail.com>
2025-07-14 09:58:58 +0700

    chore: remove docs

--- docs/assets/assets/welcome.java
+++
@@ -1,5 +0,0 @@
-/*
-💙 Thank you for using Waterbus. Let's enjoy your time.
-
-If you have suggestions or problems, please open an issue or contribute directly at https://github.com/lambiengcode/waterbus
-*/
