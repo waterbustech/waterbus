1c963ba4027cd529b0fc5c4662f2d288377c0e8e
Kai <lambiengcode@gmail.com>
2025-07-14 09:58:24 +0700

    chore: update endpoints

--- lib/core/constants/endpoints.dart
+++ lib/core/constants/endpoints.dart
@@ -1,21 +1,5 @@
 class Endpoints {
   static const String webDomain = 'https://meet.waterbus.tech';
-  // static const String baseUrl = 'https://services.waterbus.tech';
-  static const String baseUrl = 'http://localhost:5998';
+  static const String baseUrl = 'https://services.waterbus.tech';
   static const String suffixUrl = '/busapi/v3/';
-
-  // Auth
-  static const String auth = 'auth';
-
-  // Users
-  static const String users = 'users';
-  static const String username = 'users/username';
-
-  // Meetings
-  static const String meetings = 'meetings';
-  static const String joinWithPassword = 'meetings/join/password';
-  static const String joinWithoutPassword = 'meetings/join';
-
-  // Chats
-  static const String chats = 'chats';
 }
